7499a534f8ede994a43ebb8effbf2b0897b4c215
AndrewGitWorks <andrewshardanov@gmail.com>
2026-03-13 18:28:26 -0700

    Add sorting + pag + PATCH

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -34,10 +34,65 @@ namespace API.Controllers
         }
         [HttpGet]
         [Route("getall")]
-        public async Task<ICollection<ProductListResponse>> GetAllProducts([FromQuery]ProductSortModel sort)
+        public async Task<PagedResponse<ProductListResponse>> GetAllProducts([FromQuery]ProductSortModel sort)
         {
             var response = await _product.GetAllAsync();
-            return response;
+            if(sort.IsAscending && sort.IsDescending)
+            {
+                throw new ArgumentException("Cannot sort by both ascending and descending order.");
+            }
+            if(sort.IsDescending)
+            {
+                response = response.OrderByDescending(p => p.Price).ToList();
+            }
+            else if(sort.IsAscending)
+            {
+                response = response.OrderBy(p => p.Price).ToList();
+            }
+            
+            if(sort.MinPrice.HasValue)
+            {
+                response = response.Where(p => p.Price >= sort.MinPrice.Value).ToList();
+            }
+            if(sort.MaxPrice.HasValue)
+            {
+                response = response.Where(p => p.Price <= sort.MaxPrice.Value).ToList();
+            }
+            if(!string.IsNullOrWhiteSpace(sort.Search))
+            {
+                // response = response.Where(p => p.Name.Contains(sort.Search)).ToList();
+                response = (List<ProductListResponse>)response.Where(p => 
+            p.Name.Contains(sort.Search, StringComparison.OrdinalIgnoreCase));
+            }
+            var page = sort.Page ?? 1;
+            var pageSize = sort.PageSize ?? 10;
+    
+            var items = response
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+            var finalResponse = new PagedResponse<ProductListResponse>
+            {
+            Items = items,
+            TotalCount = response.Count(),
+            Page = page,
+            PageSize = pageSize
+            };
+            return finalResponse;
+        }
+        [HttpPatch]
+        [Route("/{id}/update")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] Guid id, [FromBody] UpdateProductDto request)
+        {
+            try
+            {
+                await _product.UpdateProductAsync(request, id);
+                return new OkResult();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
         [HttpPost]
         [Route("addProductToOrder")]
