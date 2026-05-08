6e9bcf456bfd4be8142c70402d78a5bb2e9386d6
AndewGitWorks <andrewshardanov@gmail.com>
2026-05-08 15:44:27 +0300

    MacInti

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// To init
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
